1679cfc210315ffb60f56236b427139bf367d16f
biopassion <awesometeam01@outlook.com>
2025-08-07 10:22:56 +0200

    update page2

--- pages/2_Metabolite_Characterization.py
+++ pages/2_Metabolite_Characterization.py
@@ -9,6 +9,8 @@ from md2docx_python.src.md2docx_python import markdown_to_word
 ## streamlit 
 import streamlit as st
 import streamlit.components.v1 as components
+import pandas as pd 
+
 
 # the current dir is where the main app.py is 
 main_dir= "./"
@@ -34,25 +36,35 @@ with st.form("my_form",clear_on_submit=False):
     
     submit_button = st.form_submit_button("Submit")
     
-    reaction_list = []
-    for reaction in model.metabolites.get_by_id(metabolite).reactions:
-        reaction_list.append([reaction.id, reaction, reaction.name])
         
     if submit_button:
         with st.expander("Result:"):
             st.markdown(f"The name of {metabolite} is:" + "\n" +model.metabolites.get_by_id(metabolite).name)
-            st.markdown(f"The compartment of {metabolite} is: \n" + model.metabolites.get_by_id(metabolite).compartment)
+            #st.markdown(f"The compartment of {metabolite} is: \n" + model.metabolites.get_by_id(metabolite).compartment)
             st.markdown(f"The formula of {metabolite} is:" + model.metabolites.get_by_id(metabolite).formula)
-            st.markdown(f"The elements of {metabolite} include:" + str(model.metabolites.get_by_id(metabolite).elements))
             st.markdown(f"The molecular weight of {metabolite} is:" + str(model.metabolites.get_by_id(metabolite).formula_weight))
-            st.markdown(f"The annotation of {metabolite} is:" + str(model.metabolites.get_by_id(metabolite).annotation))
-            st.markdown(f"The metabolite {metabolite} is involved in these reactions:" + str(model.metabolites.get_by_id(metabolite).reactions))
-            st.markdown(f"The reactions details:" + str(reaction_list))
-       
+            #st.markdown(f"The elements of {metabolite} include:" + str(model.metabolites.get_by_id(metabolite).elements))
 
-        
-        
+            st.markdown(f"The annotation of {metabolite}:" + str())
+            
+            
+            # annotaion convert to a dataframe 
+            annotaion_data = model.metabolites.get_by_id(metabolite).annotation
+            annotaion_df = pd.DataFrame.from_dict(annotaion_data, orient ="index").reset_index()
+            annotaion_df.columns = ["Database", "ID"]
+            st.dataframe(annotaion_df)
+            
+            
+            
+            #st.markdown(f"The metabolite {metabolite} is involved in these reactions:" + str(model.metabolites.get_by_id(metabolite).reactions))
+            reaction_list = []
+            for reaction in model.metabolites.get_by_id(metabolite).reactions:
+                reaction_list.append([reaction.id, reaction.name, reaction])
+                
+            st.markdown(f"{metabolite} is involved in these reactions")
+            reaction_df= pd.DataFrame(reaction_list, columns =["Reaction ID", "Reaction Name",  "Reaction"])
+            st.dataframe(reaction_df)
 
-        
+
 
 
